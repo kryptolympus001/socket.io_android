5b32a722e250fa71adf98d2d1f09fe87be3eacf1
kryptolympus001 <110574478+kryptolympus001@users.noreply.github.com>
2022-08-04 19:53:56 -0700

    Update ChatRoomActivity.kt

--- android/app/src/main/java/com/junga/socketio_android/ChatRoomActivity.kt
+++ android/app/src/main/java/com/junga/socketio_android/ChatRoomActivity.kt
@@ -57,7 +57,7 @@ class ChatRoomActivity : AppCompatActivity(), View.OnClickListener {
 
         //Let's connect to our Chat room! :D
         try {
-            mSocket = IO.socket("http://10.0.2.2:3000")
+            mSocket = IO.socket("http://192.168.43.201:3000")
             Log.d("success", mSocket.id())
 
         } catch (e: Exception) {
